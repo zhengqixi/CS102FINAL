d5d67e1f2a6c0e0ce050bc02cf483034a4e61217
zhengqi xi <zhengqixi@gmail.com>
2013-12-08 17:22:57 -0500

    Looks beautiful

--- main.c
+++ main.c
@@ -13,21 +13,23 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     printf("Zhengqi is stupid");
-	struct sockaddr_in server_addr;
-    struct sockaddr_in     client_addr;
-    
-    server_addr.sin_addr=AF_INET;
-    server_addr.sin_family.s_addr=htonl(INADDR_ANY);
-    server_addr.sin_port=1999;
-    
-    int sockfd = socket(AF_INET, SOCK_STREAM,0);
-    bind(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
-    listen(sockfd, 2);
-    int cli_size=sizeof(struct sockaddr_in); //cli_size necessary as an argument for pthread_create
-    int new_fd = accept(sockfd, (struct sockaddr *)&client_addr,&cli_size);
-    if(recv(sockfd))
-    return (EXIT_SUCCESS);
-}
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
 
+    server_addr.sin_addr = AF_INET;
+    server_addr.sin_family.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = 1999;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(sockfd, (struct sockaddr *) &server_addr,
+	 sizeof(struct sockaddr));
+    listen(sockfd, 2);
+    int cli_size = sizeof(struct sockaddr_in);	//cli_size necessary as an argument for pthread_create
+    int new_fd =
+	accept(sockfd, (struct sockaddr *) &client_addr, &cli_size);
+    if (recv(sockfd))
+	return (EXIT_SUCCESS);
+}
