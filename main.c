74b2fbe9363a5d3c3622f7a87dd497b5547a0a60
spreckel <kurutu@cooper.edu>
2013-12-11 20:20:35 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <systypes.h>
 
 /*
  * 
@@ -17,6 +18,7 @@ int main(int argc, char **argv)
 {
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
+    char[15] username;
 
     server_addr.sin_addr = AF_INET;
     server_addr.sin_family.s_addr = htonl(INADDR_ANY);
@@ -29,7 +31,9 @@ int main(int argc, char **argv)
     int cli_size = sizeof(struct sockaddr_in);	//cli_size necessary as an argument for pthread_create
     int new_fd =
 	accept(sockfd, (struct sockaddr *) &client_addr, &cli_size);
-    if (recv(sockfd))
-    	;
+    if (recv(new_fd, username,sizeof(username), 0)>0){
+    	printf("%s has joined the chat.", username);
+    }
+    
     return (EXIT_SUCCESS);
 }
